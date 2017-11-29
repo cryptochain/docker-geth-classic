4a2857838eec34b7dcc64b0436ebbd7416bb7d73
Dmitry Ananichev <a@qozz.ru>
2017-11-29 16:52:48 +0500

    genesis removed from run for now

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@
 docker run --name ethereum-classic-testbox \
   -tid -p 40303:30303 -p 9545:8545 cryptochain/ethereum-classic-docker:latest \
   --datadir=/root/.ethereum/devchain \
+  --testnet	\
+  --dev	\
+  --fast \
   --rpc \
   --rpcapi 'db,personal,eth,net,web3' \
   --rpcaddr='0.0.0.0' \
   --rpccorsdomain='*' \
   --networkid=1234 \
-  init=/root/files/genesis.json \
   --mine
